1e57c0bf3597a476a6597c49079b41ee982a0d58
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-09-26 10:37:17 +0200

    fasfasf

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,20 @@
 <?php
+use App\Http\Controllers\CustomImportController;
+use Botble\Ecommerce\Jobs\OrderSubmittedJob;
+use Botble\Ecommerce\Jobs\SendQuestionnaireToCustomerJob;
+use Botble\Ecommerce\Mail\OrderConfirmed;
+use Botble\Ecommerce\Mail\SendQuestionnaireToCustomer;
+use Botble\Ecommerce\Models\Order;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\suggestionController;
+use App\Http\Controllers\consumabiliFilterController;
+use App\Http\Controllers\CreaSconto;
+use App\Http\Controllers\FuckingRelatedBlog;
+use App\Http\Controllers\SPCController;
+use App\Http\Controllers\strumentazioniFilterController;
+use Botble\Ecommerce\Http\Controllers\OfferTypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,3 +26,8 @@
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/importposts', [CustomImportController::class, 'importPost'])
+
+    ->name('post.import');
+
+
