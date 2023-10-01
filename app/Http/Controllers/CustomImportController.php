751e2c32a2145704b6c887f1c62aa78d02c4765b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-10-01 10:44:16 +0200

    Update CustomImportController.php

--- app/Http/Controllers/CustomImportController.php
+++ app/Http/Controllers/CustomImportController.php
@@ -69,6 +69,17 @@ class CustomImportController extends BaseController
         $authors = collect(DB::connection('mysql2')->table('wp_users')->whereIn('id', $posts->pluck('post_author')->unique()->toArray())->get())->map(function ($item) {
             return (array)$item;
         })->pluck('user_email', 'ID')->toArray();
+        $post_url = "https://www.gochange.it/business/esplorando-i-lavori-nel-settore-digitale/5901";
+        $fp = file_get_contents($post_url);
+        $tags = [];
+        preg_match_all('/<img.+?class=".*?attachment-single-thumb size-single-thumb wp-post-image.*?"/', $fp, $tags);
+        $url = collect($tags)->flatten()->map(function ($item) {
+            preg_match('/<img(.*)src(.*)=(.*)"(.*)"/U', $item, $images);
+            return array_pop($images);
+        })->filter(function ($item) {
+            return filter_var($item, FILTER_VALIDATE_URL);
+        })->last();
+        dd($url);
         try {
             DB::transaction(function () use ($authors, $posts) {
                 $i=1;
