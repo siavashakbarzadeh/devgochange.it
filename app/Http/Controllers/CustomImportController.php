c4d40199b17993d06ad82dd85eb44846f548ec1a
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-10-03 11:54:07 +0200

    Update CustomImportController.php

--- app/Http/Controllers/CustomImportController.php
+++ app/Http/Controllers/CustomImportController.php
@@ -77,7 +77,7 @@ class CustomImportController extends BaseController
     {
         $url = "https://www.gochange.it/category/business";
         $dom = resolve(DOMDocument::class);
-        dd($this->file_get_contents_curl($url));
+        dd(htmlspecialchars($this->file_get_contents_curl($url)));
         @$dom->loadHTML(file_get_contents($url));
         $xpath = new DOMXpath($dom);
         $articles = $xpath->query('//div[@class="post-box-archives"] //article');
