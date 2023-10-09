252414eab9471ad28bf25c3f4c3f69e2bce254a5
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-10-09 11:36:37 +0200

    sgd

--- platform/themes/ripple/partials/header.blade.php
+++ platform/themes/ripple/partials/header.blade.php
@@ -26,7 +26,7 @@
     {!! apply_filters(THEME_FRONT_BODY, null) !!}
     <header class="header" id="header">
         <div class="header-wrap d-none d-sm-block" style="background-color: #0a53be !important;">
-            <nav class="nav-top">
+            <nav class="nav-top bg-info">
                 <div class="container">
                     <div class="row">
                         @if (theme_option('social_links') && $socialLinks = json_decode(theme_option('social_links'), true))
