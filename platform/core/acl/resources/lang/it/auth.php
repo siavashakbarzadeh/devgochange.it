65fe15e7c712144f50940bc6e63d3dfb5f9e0bd3
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2023-12-15 12:28:18 +0100

    dfwef

---
+++ platform/core/acl/resources/lang/it/auth.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    'login' => [
+        'username' => 'Email/Username',
+        'email' => 'Email',
+        'password' => 'Password',
+        'title' => 'Accesso Utente',
+        'remember' => 'Ricordami?',
+        'login' => 'Accedi',
+        'placeholder' => [
+            'username' => 'Inserisci il tuo nome utente',
+            'email' => 'Inserisci la tua email',
+        ],
+        'success' => 'Accesso effettuato con successo!',
+        'fail' => 'Nome utente o password errati.',
+        'not_active' => 'Il tuo account non è ancora stato attivato!',
+        'banned' => 'Questo account è stato bannato.',
+        'logout_success' => 'Logout effettuato con successo!',
+        'dont_have_account' => 'Non hai un account in questo sistema, contatta l\'amministratore per maggiori informazioni!',
+    ],
+    'forgot_password' => [
+        'title' => 'Password Dimenticata',
+        'message' => '<p>Hai dimenticato la tua password?</p><p>Inserisci il tuo account email. Il sistema invierà un email con un link attivo per reimpostare la tua password.</p>',
+        'submit' => 'Invia',
+    ],
+    'reset' => [
+        'new_password' => 'Nuova password',
+        'password_confirmation' => 'Conferma nuova password',
+        'email' => 'Email',
+        'title' => 'Reimposta la tua password',
+        'update' => 'Aggiorna',
+        'wrong_token' => 'Questo link è invalido o scaduto. Si prega di utilizzare nuovamente il modulo di reimpostazione.',
+        'user_not_found' => 'Questo nome utente non esiste.',
+        'success' => 'Password reimpostata con successo!',
+        'fail' => 'Token non valido, il link per la reimpostazione della password è scaduto!',
+        'reset' => [
+            'title' => 'Email per reimpostare la password',
+        ],
+        'send' => [
+            'success' => 'Un email è stata inviata al tuo account email. Si prega di controllare e completare questa azione.',
+            'fail' => 'Non è possibile inviare email in questo momento. Si prega di riprovare più tardi.',
+        ],
+        'new-password' => 'Nuova password',
+    ],
+    'email' => [
+        'reminder' => [
+            'title' => 'Email per reimpostare la password',
+        ],
+    ],
+    'password_confirmation' => 'Conferma password',
+    'failed' => 'Fallito',
+    'throttle' => 'Throttle',
+    'not_member' => 'Non sei ancora un membro?',
+    'register_now' => 'Registrati ora',
+    'lost_your_password' => 'Hai perso la tua password?',
+    'login_title' => 'Admin',
+    'login_via_social' => 'Accedi con i social network',
+    'back_to_login' => 'Torna alla pagina di accesso',
+    'sign_in_below' => 'Accedi qui sotto',
+    'languages' => 'Lingue',
+    'reset_password' => 'Reimposta Password',
+    'settings' => [
+        'email' => [
+            'title' => 'ACL',
+            'description' => 'Configurazione email ACL',
+        ],
+    ],
+];
