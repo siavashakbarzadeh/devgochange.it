584ce8f48719d81ec1f1cb1b4a8f589cca856f5e
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2023-12-16 17:45:06 +0100

    bj

---
+++ platform/plugins/member/resources/lang/it/member.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'create' => 'Nuovo membro',
+    'menu_name' => 'Membri',
+    'confirmation_subject' => 'Verifica email',
+    'confirmation_subject_title' => 'Verifica la tua email',
+    'not_confirmed' => 'L\'indirizzo email fornito non è stato confermato. <a href=":resend_link">Invia di nuovo il link di conferma.</a>',
+    'confirmation_successful' => 'Hai confermato con successo il tuo indirizzo email.',
+    'confirmation_info' => 'Si prega di confermare il proprio indirizzo email.',
+    'confirmation_resent' => 'Ti abbiamo inviato un\'altra email di conferma. La riceverai a breve.',
+    'form' => [
+        'email' => 'Email',
+        'password' => 'Password',
+        'password_confirmation' => 'Conferma password',
+        'change_password' => 'Cambiare la password?',
+    ],
+    'forgot_password' => 'Password dimenticata',
+    'login' => 'Accedi',
+    'settings' => [
+        'email' => [
+            'title' => 'Membro',
+            'description' => 'Configurazione email del membro',
+        ],
+    ],
+    'first_name' => 'Nome',
+    'last_name' => 'Cognome',
+    'email_placeholder' => 'Esempio: example@gmail.com',
+    'write_a_post' => 'Scrivi un post',
+    'phone' => 'Telefono',
+    'phone_placeholder' => 'Telefono',
+    'confirmed_at' => 'Confermato il',
+    'avatar' => 'Avatar',
+    'dob' => 'Data di nascita',
+];
+
